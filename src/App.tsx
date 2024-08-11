6f36344782fb82cf0ddd34ba6ad4be440d6b84f7
a-sh-dev <iam.angie@ymail.com>
2024-08-11 16:37:30 +1000

    Configure basic styling - Bellroy look

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ function App() {
   return (
     <>
       <div>Hello World</div>
+      <p>
+        Rational yet playful. Practical while progressive. Bellroy is a unique
+        blend of art and science. We constantly seek effective ways to improve,
+        work to positively impact the world and believe that diversity inspires
+        greater insights. Sound interesting?
+      </p>
     </>
   )
 }
