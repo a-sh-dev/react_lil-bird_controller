793f807b1591681830646eb11ece6973883e9645
a-sh-dev <iam.angie@ymail.com>
2024-08-12 00:54:26 +1000

    Create basic controller for buttons

---
+++ src/components/Controller.tsx
@@ -0,0 +1,10 @@
+export const Controller = () => {
+  return (
+    <div>
+      <button>left</button>
+      <button>up</button>
+      <button>down</button>
+      <button>right</button>
+    </div>
+  )
+}
