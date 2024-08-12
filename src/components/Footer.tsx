93eccb53480b5503b34e861b8f1c0979b1f3de92
a-sh-dev <iam.angie@ymail.com>
2024-08-12 21:16:34 +1000

    Move css to sc for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,18 @@
+import styled from 'styled-components'
+import { sharedStyles as s } from '../styles/sharedStyles'
+
+const StyledFooter = styled.footer`
+  padding: 0.8rem 0;
+  font-size: ${s.text.sm};
+  text-align: center;
+  color: ${s.palette.grey[300]};
+  background-color: var(--color-grey-600);
+  width: 100dvw;
+`
+
 export const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       Crafted with heart by{' '}
       <a
         href="https://github.com/a-sh-dev/react_lil-bird_controller"
@@ -9,6 +21,6 @@ export const Footer = () => {
         a-sh.
       </a>{' '}
       in 2024
-    </footer>
+    </StyledFooter>
   )
 }
